7260c8577947f208338658e1bce96d1c736078ab
Fiqri Taufiqq <46043858+Qiri009@users.noreply.github.com>
2020-08-08 15:58:37 +0800

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    public Animator animator;
+     
+    // Update is called once per frame
+    void Update()
+    {
+        
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump")) 
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }   
+
+
+
+    }
+
+    public void OnLanding ()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+    void FixedUpdate ()
+    {
+        //this is where we want to move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
